c7e67a9372f3b13c551c0e7ba5fbb53eeada91c0
Miguel Iglesias <miguel.iglesias@dimeder.com>
2020-12-07 15:05:05 -0500

    names

--- 2020/day2.rkt
+++ 2020/day2.rkt
@@ -10,14 +10,14 @@
 
 (define valid_passwords (count (λ (triplet)
         (match triplet 
-          [(list range letter pass)
-           (let* ([r (map (λ (s) (string->number s)) (string-split (symbol->string range) "-"))]
-                  [lower (car r)]
-                  [upper (car (cdr r))]
-                  [l (substring (symbol->string letter) 0 1) ]
+          [(list range letterRAW pass)
+           (let* ([range (map (λ (s) (string->number s)) (string-split (symbol->string range) "-"))]
+                  [lower (car range)]
+                  [upper (car (cdr range))]
+                  [letter (substring (symbol->string letterRAW) 0 1) ]
                   [groups (group-by (λ (s) s) (string-split (symbol->string pass) ""))]
                   [counts (map (λ (group) (cons (car group) (length group))) groups)]
-                  [letter_count (filter-map (λ (c) (and (equal? (car c) l) (cdr c) )) counts)])
+                  [letter_count (filter-map (λ (c) (and (equal? (car c) letter) (cdr c) )) counts)])
              (match letter_count 
                [(list c) (and (<= lower c) (>= upper c))]
                [(list) #f]))
