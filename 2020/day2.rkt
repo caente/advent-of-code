0b99a35013775c688fbdf6f7bda547ef719137cc
Miguel Iglesias <miguel.iglesias@dimeder.com>
2020-12-07 22:17:16 -0500

    day2 done

--- 2020/day2.rkt
+++ 2020/day2.rkt
@@ -5,10 +5,11 @@
 (require racket/match)
 (require algorithms)
 (require racket/string)
+(require racket/bool)
 
 (define input (file->list "2020/input_day2.txt"))
 
-(define valid_passwords (count (λ (triplet)
+(define valid_passwords_wrong (count (λ (triplet)
         (match triplet 
           [(list range letter password)
            (let* ([range (map (λ (s) (string->number s)) (string-split (symbol->string range) "-"))]
@@ -21,6 +22,21 @@
              (match letter_count 
                [(list c) (and (<= lower c) (>= upper c))]
                [(list) #f]))
+           ]))
+     (sliding input 3 3)))
+
+(define valid_passwords (count (λ (triplet)
+        (match triplet
+          [(list range letter pass)
+           (let* ([range (map (λ (s) (string->number s)) (string-split (symbol->string range) "-"))]
+                  [lower (car range)]
+                  [upper (car (cdr range))]
+                  [letter (substring (symbol->string letter) 0 1) ]
+                  [password (list->vector (filter (λ (s) (not (equal? s ""))) (string-split (symbol->string pass) "")))])
+             (let* ([a (vector-ref password (sub1 lower))]
+                    [pass_ln (vector-length password)]
+                    [b (and (<= upper pass_ln) (vector-ref password (sub1 upper)))])
+               (xor (equal? a letter) (equal? b letter))))
            ]))
      (sliding input 3 3)))
 
